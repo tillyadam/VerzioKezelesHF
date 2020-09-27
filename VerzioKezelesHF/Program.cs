01329aeec6ae7a846a8204a2842804678fc6e16e
Ádám Tilly <tilly.adam@gmail.com>
2020-09-27 14:17:42 +0200

    10 db szam bekerese es tomb feltoltese

--- VerzioKezelesHF/Program.cs
+++ VerzioKezelesHF/Program.cs
@@ -11,6 +11,12 @@ namespace VerzioKezelesHF
         static void Main(string[] args)
         {
             double[] szamok = new double[10];
+
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                Console.Write("Adjon meg egy szamot a(z) {0}. szamot: ",i+1);
+                szamok[i] = double.Parse(Console.ReadLine());
+            }
         }
     }
 }
