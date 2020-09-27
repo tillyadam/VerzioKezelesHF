ab1a95d2c1fe16f07b4b8913fbeb118e3bbc239e
Ádám Tilly <tilly.adam@gmail.com>
2020-09-27 14:22:08 +0200

    Atlag kiirasa

--- VerzioKezelesHF/Program.cs
+++ VerzioKezelesHF/Program.cs
@@ -11,12 +11,15 @@ namespace VerzioKezelesHF
         static void Main(string[] args)
         {
             double[] szamok = new double[10];
-
+            
             for (int i = 0; i < szamok.Length; i++)
             {
                 Console.Write("Adjon meg egy szamot a(z) {0}. szamot: ",i+1);
                 szamok[i] = double.Parse(Console.ReadLine());
             }
+
+            Console.WriteLine("A megadott szamok atlaga: "+szamok.Sum()/szamok.Length);
+            Console.ReadKey();
         }
     }
 }
